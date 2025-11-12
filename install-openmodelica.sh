ca98e9a416faa1b3119f47a5c578f6e13f32a791
Gustavo Quiros A. <gustavoquiros@gmail.com>
2025-11-11 23:05:01 -0500

    Updated install commands from OM website

--- install-openmodelica.sh
+++ install-openmodelica.sh
@@ -1,18 +1,13 @@
 
 sudo apt-get update
 sudo apt-get install ca-certificates curl gnupg
+sudo curl -fsSL http://build.openmodelica.org/apt/openmodelica.asc | \
+  sudo gpg --dearmor -o /usr/share/keyrings/openmodelica-keyring.gpg
 
-curl -fsSL http://build.openmodelica.org/apt/openmodelica.asc | sudo tee /etc/apt/trusted.gpg.d/openmodelica-keyring.asc
-
-sudo chmod a+r /etc/apt/trusted.gpg.d/openmodelica-keyring.asc
-
-echo "deb [arch=amd64 signed-by=/etc/apt/trusted.gpg.d/openmodelica-keyring.asc] \
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/openmodelica-keyring.gpg] \
   https://build.openmodelica.org/apt \
   $(cat /etc/os-release | grep "\(UBUNTU\\|DEBIAN\\|VERSION\)_CODENAME" | sort | cut -d= -f 2 | head -1) \
   stable" | sudo tee /etc/apt/sources.list.d/openmodelica.list
-  
+
 sudo apt update && sudo apt install openmodelica
-  
-#gpg --keyserver keyserver.ubuntu.com --recv-keys 3A59B53664970947
-#gpg --export --armor 3A59B53664970947 | sudo tee /etc/apt/trusted.gpg.d/openmodelica-keyring.asc && sudo apt-get update 
 
